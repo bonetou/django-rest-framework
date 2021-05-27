ceabbc0fc89012fe1c5505e4e4e912faa8b32312
Henrique Boneto <h.boneto@outlook.com>
2021-05-27 15:19:19 -0300

    serializers

---
+++ school/serializer.py
@@ -0,0 +1,13 @@
+from django.db.models import fields
+from rest_framework import serializers
+from school.models import Student, Course
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'student_number']
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
