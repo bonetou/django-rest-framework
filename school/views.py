6559112ba3a14e23c9f4048f1d93265e0398ef1b
Henrique Boneto <h.boneto@outlook.com>
2021-05-27 18:17:53 -0300

    viewsets

--- school/views.py
+++ school/views.py
@@ -1,9 +1,15 @@
-from django.http import JsonResponse
+from django.db.models import query
+from rest_framework import serializers, viewsets
+from school.models import Student, Course
+from .serializer import StudentSerializer, CourseSerializer
 
-def students(request):
-    if request.method == 'GET':
-        student = {
-            'id': 1,
-            'name': 'John',
-        }
-        return JsonResponse(student)
+class StudentViewSet(viewsets.ModelViewSet):
+    '''Shows all students'''
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    '''Show all courses'''
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    
